5734845b83b96a22bbdcdd8ee2df091c8fde089c
rose030@gannon.edu <rose030@gannon.edu>
2023-04-12 15:54:32 -0400

    good enough for now

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,55 +1,101 @@
 import React, { useState } from 'react';
-import '../css/MainPage.css';
+import {
+  Box,
+  Container,
+  Typography,
+  TextField,
+  Button,
+  Checkbox,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  IconButton,
+} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState('');
 
-function TodoList() {
-    const [items, setItems] = useState([]);
-    const [text, setText] = useState('');
-  
-    const handleChange = (e) => {
-      setText(e.target.value);
-    };
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      if (!text.trim()) return;
-      setItems([...items, { text, id: Date.now(), completed: false }]);
-      setText('');
-    };
-  
-    const handleComplete = (id) => {
-      setItems(
-        items.map((item) =>
-          item.id === id ? { ...item, completed: !item.completed } : item
-        )
-      );
-    };
-  
-    return (
-      <div className="TodoList">
-        <h1>To-Do List</h1>
-        <form onSubmit={handleSubmit}>
-          <input type="text" value={text} onChange={handleChange} placeholder="Add a new task" />
-          <button type="submit">Add Task</button>
-        </form>
-        <ul>
-          {items.map((item) => (
-            <li key={item.id}>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={item.completed}
-                  onChange={() => handleComplete(item.id)}
-                />
-                <span className={item.completed ? 'completed' : 'uncompleted'}>
-                  {item.text}
-                </span>
-              </label>
-            </li>
+  const handleTaskInputChange = (e) => {
+    setTaskInput(e.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (taskInput.trim() !== '') {
+      setTasks([...tasks, { text: taskInput, completed: false }]);
+      setTaskInput('');
+    }
+  };
+
+  const handleTaskCompletion = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
+  };
+
+  const handleDeleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <Box sx={{ bgcolor: '#f5f5f5', minHeight: '100vh', color: '#333' }}>
+      <Container sx={{ py: 8 }}>
+        <Typography variant="h2" sx={{ mb: 4 }}>
+          Todo List
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          sx={{ display: 'flex', alignItems: 'flex-end' }}
+        >
+          <TextField
+            label="New Task"
+            variant="outlined"
+            size="small"
+            fullWidth
+            value={taskInput}
+            onChange={handleTaskInputChange}
+            sx={{ '& .MuiOutlinedInput-notchedOutline': { borderColor: '#ccc' } }}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ ml: 2 }}
+            onClick={handleAddTask}
+          >
+            Add
+          </Button>
+        </Box>
+        <List sx={{ mt: 4 }}>
+          {tasks.map((task, index) => (
+            <ListItem key={index} disablePadding sx={{ fontSize: '1.5rem' }}>
+              <ListItemButton onClick={() => handleTaskCompletion(index)}>
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={task.completed}
+                    tabIndex={-1}
+                    disableRipple
+                  />
+                </ListItemIcon>
+                <ListItemText primary={task.text} sx={{ color: '#333' }} />
+              </ListItemButton>
+              <IconButton onClick={() => handleDeleteTask(index)}>
+                <DeleteIcon sx={{ color: '#333' }} />
+              </IconButton>
+            </ListItem>
           ))}
-        </ul>
-      </div>
-    );
-  }
-  
-  export default TodoList;
+        </List>
+      </Container>
+    </Box>
+  );
+};
+
+export default TodoList;
