c22218e5828bfdc7d92339398312af977f8334a4
rose030@gannon.edu <rose030@gannon.edu>
2023-04-12 15:38:32 -0400

    made it darker

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,50 +1,55 @@
 import React, { useState } from 'react';
 import '../css/MainPage.css';
 
+
 function TodoList() {
-  const [items, setItems] = useState([]);
-  const [text, setText] = useState('');
-
-  const handleChange = (e) => {
-    setText(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!text.trim()) return;
-    setItems([...items, { text, id: Date.now(), completed: false }]);
-    setText('');
-  };
-
-  const handleComplete = (id) => {
-    setItems(
-      items.map((item) =>
-        item.id === id ? { ...item, completed: !item.completed } : item
-      )
+    const [items, setItems] = useState([]);
+    const [text, setText] = useState('');
+  
+    const handleChange = (e) => {
+      setText(e.target.value);
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (!text.trim()) return;
+      setItems([...items, { text, id: Date.now(), completed: false }]);
+      setText('');
+    };
+  
+    const handleComplete = (id) => {
+      setItems(
+        items.map((item) =>
+          item.id === id ? { ...item, completed: !item.completed } : item
+        )
+      );
+    };
+  
+    return (
+      <div className="TodoList">
+        <h1>To-Do List</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={text} onChange={handleChange} placeholder="Add a new task" />
+          <button type="submit">Add Task</button>
+        </form>
+        <ul>
+          {items.map((item) => (
+            <li key={item.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={item.completed}
+                  onChange={() => handleComplete(item.id)}
+                />
+                <span className={item.completed ? 'completed' : 'uncompleted'}>
+                  {item.text}
+                </span>
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
-  };
-
-  return (
-    <div className="TodoList">
-      <h1>To-Do List</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={text} onChange={handleChange} placeholder="Add a new task" />
-        <button type="submit">Add Task</button>
-      </form>
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>
-            <span
-              className={item.completed ? 'completed' : 'uncompleted'}
-              onClick={() => handleComplete(item.id)}
-            >
-              {item.text}
-            </span>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-export default TodoList;
+  }
+  
+  export default TodoList;
