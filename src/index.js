1fc7bcbfe16d4eb72cd1b1ba82451377f9848926
rose030@gannon.edu <rose030@gannon.edu>
2023-04-20 12:31:08 -0400

    simpe login page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 import TodoList from './components/MainPage';
+import Login from './components/Login';
+
+
+const Routing = () => {
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/main" element={<TodoList />} />
+      </Routes>
+    </Router>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
-     <TodoList />
-  </React.StrictMode>
+    <Routing />
+  </React.StrictMode>,
 );
